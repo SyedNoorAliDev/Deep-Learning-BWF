0f757402601cb620c0e6340f72cc20efcd8bac00
SyedNoorAliDev <syednshah5@gmail.com>
2023-03-26 11:15:28 +0500

    @3rd taskstatus

---
+++ Task-2.py
@@ -0,0 +1,34 @@
+#Store a message in a variable, and then print that message.
+message = "Hello World"
+print(message)
+#Store a message in a variable, and print that message. Then change the value of your variable to a new message, and print the new message.
+msg = "message"
+print(msg)
+msg = "message1"
+print(msg)
+#Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+name = "Adil"
+print("Hello ",name,", would you like to learn some python today?")
+#Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+print(name.upper())
+print(name.lower())
+print(name.title())
+#Find a quote from a famous person you admire. Print the quote and the name of its author.
+print("Albert Einstein once said, “A person who never made a mistake never tried anything new.”")
+#Repeat Exercise 2-5, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+print(name," once said, “A person who never made a mistake never tried anything new.”")
+#Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use eachcharacter combination, "\t" and "\n", at least once, use lstrip(),rstrip(), and strip().
+name1 = "\tALI\n"
+print(name1)
+print(name1.rstrip())
+print(name1.lstrip())
+print(name1.strip())
+#Write addition, subtraction, multiplication, and division operations that each result in the number 8.
+print(4+4)
+print(10-2)
+print(4*2)
+print(int(16/2))
+#Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+num = 10
+print("Fav num is ",num)
+#Syed Noor Ali
